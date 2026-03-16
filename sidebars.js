f0627f8a7929816f9d4fbdc612779b9383333a0d
Private@1647 <shantanuw@lambdatest.com>
2026-03-16 16:03:08 +0530

    create pr in KaneAI doc

--- sidebars.js
+++ sidebars.js
@@ -1418,6 +1418,7 @@ module.exports = {
           "kane-ai-test-plan",
           "kaneai-hyperexecute-test-run-execution",
           "kaneai-ci-cd-automation",
+          "kaneai-create-pr",
           "test-runs-configurations",
           "kaneai-scheduled-test-runs",
         ],
