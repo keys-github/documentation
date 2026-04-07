4b5f3ca502aaa44c9a8a75d12187c7a17938d662
akshayverma <akshayverma@lamdatest.com>
2026-04-08 01:16:34 +0530

    Contacts Support

--- sidebars.js
+++ sidebars.js
@@ -3133,6 +3133,7 @@ module.exports = {
               "audio-injection",
               "regular-expression-appium",
               "appium-upload-media",
+              "appium-upload-contacts",
               "network-configurations",
               "app-auto-network-throttling",
               "sim-support",
