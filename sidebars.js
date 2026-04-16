4e7ae5f38e6db13e6978c8ad1d99dc19ca47991e
Isha vyas <72408461+Ishavyas9@users.noreply.github.com>
2026-04-16 13:54:19 +0530

    Merge pull request #2825 from RushilK7/docs/hide-issues-release
    Add Hide and Restore Issues accessibility documentation.

--- sidebars.js
+++ sidebars.js
@@ -2132,6 +2132,7 @@ module.exports = {
           "accessibility-capture-screenshot",
           "accessibility-tag-support",
           "accessibility-fragment-identifier",
+          "accessibility-hide-restore-issues",
           "accessibility-web-score"
         ],
       },
