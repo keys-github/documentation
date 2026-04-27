a46b6a3e0c97ad12400771368d872e35b0178388
salmank <salmank@lambdatest.com>
2026-04-27 16:27:29 +0530

    update

--- sidebars.js
+++ sidebars.js
@@ -2967,7 +2967,6 @@ module.exports = {
         label: "Connect Your Tools",
         items: [
           "selenium-bidi-integration",
-          "automation-mcp-server",
           "lambda-hooks",
           "command-annotations",
           "selenium-add-test-meta-data",
